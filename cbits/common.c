1e571889fe4d615985f6ca44263c1f82e3cc2594
Artem Chirkin <achirkin@users.noreply.github.com>
2022-01-03 11:07:11 +0100

    A bit safer name generation

--- cbits/common.c
+++ cbits/common.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 #include <limits.h>
+#include <stdatomic.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
@@ -14,19 +15,25 @@
 #define GetMyPid (int)getpid()
 #endif
 
-static int _unique_seed = 0;
-static int _my_pid = 0;
+static atomic_int process_seed = 0;
+static _Thread_local bool initialized = false;
+static _Thread_local int process_id = 0;
 static const char keytable[] =
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 static const char prefix[] = "/HsIPC.";
 #define keytableLength 62
 #define prefixLength 7
+// Leave a few symbol placeholders empty to be able to append some
+// short suffixes to the object name in-place.
+#define suffixLength 1
 
 void genSharedObjectName(char* const name) {
-  // Init this once per process
-  if (_unique_seed == 0) {
+  int seed = atomic_fetch_add_explicit(&process_seed, 1, memory_order_relaxed);
+  // Init this once per thread
+  if (!initialized) {
+    initialized = true;
     srand(time(NULL));
-    _my_pid = GetMyPid;
+    process_id = GetMyPid;
   }
   // Clear the name
   memset(name, 0, sizeof(SharedObjectName));
@@ -34,10 +41,10 @@ void genSharedObjectName(char* const name) {
   // Combine three sources of uniqueness
   const int rands_num = 3;
   div_t rands[] = {{.quot = rand() ^ 0x19a628f6, .rem = 0},
-                   {.quot = _my_pid ^ 0x003772a1, .rem = 0},
-                   {.quot = (_unique_seed++) ^ 0x028ab100, .rem = 0}};
+                   {.quot = process_id ^ 0x003772a1, .rem = 0},
+                   {.quot = seed ^ 0x028ab100, .rem = 0}};
   for (int i = prefixLength, j = 0, k = 0;
-       i < SHARED_OBJECT_NAME_LENGTH - 1 && k < rands_num;
+       i < SHARED_OBJECT_NAME_LENGTH - 1 - suffixLength && k < rands_num;
        i++, j = (j + 1) % rands_num) {
     rands[j] = div(rands[j].quot, keytableLength);
     name[i] = keytable[rands[j].rem];

89b34110de8bfa9b4cbc22cfa0c1db1ce617f4dc
Artem Chirkin <achirkin@users.noreply.github.com>
2022-01-08 15:51:34 +0100

    Refactor tests

--- test/StoredMVar.hs
+++ test/StoredMVar.hs
@@ -1,27 +1,11 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE TupleSections #-}
 module Main (main) where
 
-import           Control.Concurrent                    (forkIO)
-import qualified Control.Concurrent.MVar               as Vanilla
+import           Control.Concurrent                    (threadDelay)
+import           Control.Concurrent.Async
 import           Control.Concurrent.Process.StoredMVar
-import           Control.Exception
-import           Control.Monad                         (forM, void)
-import           Data.Monoid
-import           Foreign.SharedObjectName
-import           System.Environment
-import           System.Exit
-import           System.IO
-import           System.IO.Unsafe
-import           System.Mem
-import           System.Process.Typed
-import           Text.Read                             (readMaybe)
-
--- | A number of processes trying to do something concurrently.
---   For example, read from the same StoredMVar.
---   Actual number of processes may vary depending on a test case.
---   Minimum allowed number is 1.
-concProcBaseN :: Int
-concProcBaseN = 20
+import           Tools.Runner
+import           Tools.TestResult
 
 data BasicRole = Master | Slave
   deriving (Eq, Ord, Show, Read)
@@ -29,142 +13,73 @@ data BasicRole = Master | Slave
 data ThreeWayRole = Reader | Taker | Putter
   deriving (Eq, Ord, Show, Read)
 
-data TestSpec
-  = SimpleTakePut (SOName (StoredMVar Double)) BasicRole
-  | ReadersTakers (SOName (StoredMVar Double)) ThreeWayRole
-  deriving (Eq, Ord, Show, Read)
-
-
-run :: TestSpec -> IO TestResult
-
-run (SimpleTakePut ref Master) = do
-  mVar <- lookupMVar ref
-  putMVar mVar 42
-  putMVar mVar 17
-  return Success
-run (SimpleTakePut ref Slave) = do
-  mVar <- lookupMVar ref
-  a <- takeMVar mVar
-  b <- takeMVar mVar
-  return $ if (a + b) == (42 + 17)
-           then Success
-           else Failure $ show (a + b) ++ " /= 42 + 17"
-
-run (ReadersTakers ref Putter) = do
-  mVar <- lookupMVar ref
-  putMVar mVar 177
-  putMVar mVar 178
-  putMVar mVar 179
-  putMVar mVar 777
-  return Success
-run (ReadersTakers ref Taker) = do
-  mVar <- lookupMVar ref
-  a <- takeMVar mVar
-  b <- takeMVar mVar
-  c <- takeMVar mVar
-  putStrLn $ "Taking: " ++ show (a,b,c)
-  return $ if a < b && b < c
-           then Success
-           else Failure "Three taken numbers must go ordered!"
-run (ReadersTakers ref Reader) = do
-  mVar <- lookupMVar ref
-  a <- readMVar mVar
-  b <- readMVar mVar
-  c <- readMVar mVar
-  putStrLn $ "Reading: " ++ show (a,b,c)
-  return $ if a <= b && b <= c
-           then Success
-           else Failure "Three taken numbers must go ordered!"
-
-tests :: [IO ([TestSpec], IO ())]
-tests =
-  [ do
-    mVar <- newEmptyMVar
-    return
-      ( [ SimpleTakePut (mVarName mVar) Slave
-        , SimpleTakePut (mVarName mVar) Master
-        ]
-      , return (mVar `seq` ())
-      )
-  , do
-    mVar <- newEmptyMVar
-    return
-      ( replicate concProcBaseN (ReadersTakers (mVarName mVar) Reader)
-        ++
-        [ ReadersTakers (mVarName mVar) Taker
-        , ReadersTakers (mVarName mVar) Putter
-        ]
-      , return (mVar `seq` ())
-      )
+simpleTakePut :: TestSpec
+simpleTakePut = TestSpec "SimpleTakePut"
+  [ (Master, run)
+  , (Slave, run)
   ]
+  where
+    run :: BasicRole -> StoredMVar Double -> IO TestResult
+    run Master mVar = do
+      putMVar mVar 42
+      putMVar mVar 17
+      return Success
+    run Slave mVar = do
+      a <- takeMVar mVar
+      b <- takeMVar mVar
+      return $
+        if (a + b) == (42 + 17)
+          then Success
+          else Failure $ show (a + b) ++ " /= 42 + 17"
 
 
+readersTakers :: TestSpec
+readersTakers = TestSpec "ReadersTakers" $
+    (, run) <$> (replicate 20 Reader <> [Taker, Putter])
+  where
+    run :: ThreeWayRole -> StoredMVar Int -> IO TestResult
+    run Putter mVar = do
+      let putLoud x = do
+            putStrLn $ "Putting " ++ show x ++ "..."
+            putMVar mVar x
+      putLoud 177
+      putLoud 178
+      putLoud 179
+      putLoud 777
+      return Success
+    run Taker mVar = do
+      a <- takeMVar mVar
+      b <- takeMVar mVar
+      c <- takeMVar mVar
+      putStrLn $ "Taking: " ++ show (a, b, c)
+      return $
+        if a < b && b < c
+          then Success
+          else Failure "Three taken numbers must go ordered!"
+    run Reader mVar = do
+      a <- readMVar mVar
+      b <- readMVar mVar
+      c <- readMVar mVar
+      putStrLn $ "Reading: " ++ show (a, b, c)
+      return $
+        if a <= b && b <= c
+          then Success
+          else Failure "Three taken numbers must go ordered!"
 
-data TestResult
-  = Success
-  | Failure String
-  deriving (Eq, Ord, Show, Read)
 
-instance Semigroup TestResult where
-  Success <> a           = a
-  Failure s <> Success   = Failure s
-  Failure s <> Failure t = Failure $ unlines [s,t]
-
-instance Monoid TestResult where
-  mempty = Success
-
-displayResult :: TestResult -> String
-displayResult Success = "OK."
-displayResult (Failure s) = unlines $ ("Failure":) . map (mappend "  ") . filter (not . null) $ lines s
-
-finish :: TestResult -> IO a
-finish Success     = exitSuccess
-finish (Failure s) = die s
+asyncException :: TestSpec
+asyncException = TestSpec "AsyncException" [((), run)]
+  where
+    run :: () -> StoredMVar Int -> IO TestResult
+    run _ mvar = do
+      locked <- async $ takeMVar mvar
+      threadDelay (55000 :: Int)
+      killed <- async $ cancel locked
+      threadDelay (55000 :: Int)
+      r <- poll killed
+      return $ case r of
+        Nothing -> Failure "The thread did not finish in time."
+        Just _  -> Success
 
 main :: IO ()
-main = do
-    execFile <- getExecutablePath
-    args <- getArgs
-    case getFirst $ foldMap (First . readMaybe) args of
-      -- run particular test routine
-      Just spec -> run spec >>= finish
-      -- run all tests
-      Nothing -> do
-        results <- forM tests $ \iox -> do
-          (ts, fin) <- iox
-          r <-runSpecs execFile ts
-          fin
-          return r
-        case foldMap id results of
-          Success   -> exitSuccess
-          Failure _ -> exitFailure
-
-runSpecs :: FilePath -> [TestSpec] -> IO TestResult
-runSpecs f specs = do
-    putStrLn ""
-    r <- go (zip [1 :: Int ..] specs) >>= evaluate
-    performGC
-    putStrLn $ "Result: " ++ displayResult r
-    return r
-  where
-    conf ts = setStdout createPipe
-            $ setStderr createPipe
-            $ proc f [show ts]
-
-    go [] = return Success
-    go ((i,x):xs) = do
-        mr <- Vanilla.newEmptyMVar
-        void . forkIO $ withProcessWait (conf x) $ \p -> do
-          hGetContents (getStdout p) >>= mapM_ (putStrLn . withI) . lines
-          errs <- hGetContents (getStderr p)
-          ecode <- waitExitCode p
-          evaluate $ foldr seq () errs
-          Vanilla.putMVar mr $! case ecode of
-            ExitSuccess   -> Success
-            ExitFailure _ -> Failure (unlines . map withI $ lines errs)
-
-        rx <- unsafeInterleaveIO $ Vanilla.takeMVar mr
-        rxs <- go xs
-        return $ mappend rx rxs
-      where
-        withI s = "[" ++ show i ++ "] " ++ s
+main = runTests [simpleTakePut, readersTakers, asyncException]
---
+++ test/Tools/Runner.hs
@@ -0,0 +1,125 @@
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Tools.Runner (runTests, TestSpec (..)) where
+
+import           Control.Concurrent.Async
+import qualified Control.Concurrent.Chan               as Chan
+import           Control.Concurrent.Process.StoredMVar
+import           Control.Exception                     (catch)
+import           Control.Monad                         (forever)
+import           Data.Foldable                         (fold)
+import qualified Data.List                             as List
+import qualified Data.Map                              as Map
+import           Data.Tuple                            (swap)
+import           Foreign.Storable
+import           System.Environment                    (getArgs,
+                                                        getExecutablePath)
+import           System.Exit
+import           System.IO
+import           System.Mem                            (performGC)
+import           System.Process.Typed
+import           Text.Read                             (readMaybe)
+
+import           Tools.TestResult
+
+{- |
+  Specification of a single test.
+  Defines the test name and the list of roles to be given to subprocesses.
+
+  The test spawns as many subprocesses as there are elements in the roles list.
+  All of them share a single IPC MVar for communication. In addition, the role
+  actions are parameterized by the role key `k`, which is passed via the command-line
+  arguments using the Show-Read instances.
+ -}
+data TestSpec where
+  TestSpec :: forall k ctx
+            . (Show k, Read k, Eq k, Storable ctx)
+           => String -> [(k, k -> StoredMVar ctx -> IO TestResult)] -> TestSpec
+
+-- | Get the name of the test.
+testName :: TestSpec -> String
+testName (TestSpec name _) = name
+
+-- | Run all tests in the list. Use it as the `main`:
+--
+-- @
+--   main :: IO ()
+--   main = runTests [...]
+-- @
+runTests :: [TestSpec] -> IO ()
+runTests specs = do
+  execFile <- getExecutablePath
+  args <- getArgs
+  case args of
+    -- run all tests
+    [] -> do
+        results <- mapM (runSpec execFile) specs
+        case fold results of
+            Success   -> exitSuccess
+            Failure _ -> exitFailure
+
+    -- run particular test routine
+    [n, sk, sm] -> do
+        hSetBuffering stdout LineBuffering
+        TestSpec _ roles <- case List.find ((== n) . testName) specs of
+            Nothing   -> fail $ "Unknown test name: " ++ show n
+            Just spec -> pure spec
+        k <- case readMaybe sk of
+            Nothing -> fail $ "Couldn't read the role " ++ show sk
+            Just x  -> pure x
+        m <- case readMaybe sm of
+            Nothing  -> fail $ "Couldn't read the mvar name " ++ show sm
+            Just ref -> lookupMVar ref
+        case List.find ((== k) . fst) roles of
+            Nothing -> fail $ "Unknown test role " ++ show k ++ " for test " ++ show n
+            Just (_ , action) -> action k m >>= finish
+
+    -- unexpected
+    _ -> fail $ "Expected zero or three arguments, got " ++ show args
+
+
+runSpec :: FilePath -> TestSpec -> IO TestResult
+runSpec f (TestSpec name (roles :: [(k, k -> StoredMVar ctx -> IO TestResult)])) = do
+    putStrLn $ "[" ++ name ++ "] Running..."
+    childIns <- Chan.newChan
+    comms <- newEmptyMVar :: IO (StoredMVar ctx)
+    let k = show $ mVarName comms
+    r <- withAsync (forever $ Chan.readChan childIns >>= printChildLine) $ \_ -> do
+        r <- runConcurrently $ foldMap (\(l, r) -> go childIns l [name, r, k]) labelsroles
+        r `seq` comms `seq` performGC
+        return r
+    putStrLn $ "[" ++ name ++ "] Result: " ++ displayResult r
+    return r
+  where
+    roleCount :: Map.Map String Int
+    roleCount = Map.filter (> 1) $
+      List.foldl' (\m (k, _) -> Map.alter (Just . maybe 1 succ) (show k) m) mempty roles
+
+    labelsroles :: [(String, String)]
+    labelsroles = snd $ List.mapAccumR (\m (k, _) -> swap $ Map.alterF (
+      -- Maybe Int -> ((String, String), Maybe Int)
+      maybe ((show k, show k), Nothing) (\i -> ((show k ++ "-" ++ show i, show k), Just (pred i)))
+      ) (show k) m ) roleCount roles
+    
+    withLabel label s = "  [" ++ label ++ "] " ++ s
+
+    printChildLine (label, s) = putStrLn $ withLabel label s
+    
+    channelHandle sink i handle = forever
+      (hGetLine handle >>= \s -> Chan.writeChan sink (i, s))
+        `catch` (const mempty :: IOError -> IO ())
+
+    mkConf x = setStdout createPipe $
+      setStderr createPipe $ proc f x
+
+    go stdSink label x = Concurrently $ withProcessWait (mkConf x) $ \p ->
+      withAsync (channelHandle stdSink label (getStdout p)) $ \sink -> do
+          ecode <- waitExitCode p
+          r <- case ecode of
+            ExitSuccess   -> return Success
+            ExitFailure _ -> do
+              errs <- hGetContents (getStderr p)
+              return $ Failure (unlines . map (withLabel label) $ lines errs)
+          wait sink
+          return r

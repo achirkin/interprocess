b0fc37d1fec1aef38ed3abbe1075905c1f0d24f8
achirkin <achirkin@users.noreply.github.com>
2022-06-12 18:04:23 +0200

    Redirect output to a temp file instead of process:createPipe

--- test/Tools/Handle.hs
+++ test/Tools/Handle.hs
@@ -1,69 +1,42 @@
+{-# LANGUAGE LambdaCase #-}
 module Tools.Handle
     ( redirectHandle
     ) where
 
-import Control.Exception       (bracket)
-import GHC.IO.Handle           (hDuplicate, hDuplicateTo)
-import GHC.IO.Handle.Internals (withHandle_)
-import GHC.IO.Handle.Types     ( Handle(FileHandle, DuplexHandle), Handle__(haInputNL, haOutputNL) )
+import Control.Exception   (bracket)
+import GHC.IO.Handle       (hDuplicate, hDuplicateTo, hFlushAll)
+import GHC.IO.Handle.Types (Handle (DuplexHandle, FileHandle))
 import System.IO
-import System.Process          (createPipe)
+import System.IO.Temp      (withSystemTempFile)
 
 
 -- | Temporary redirect the output of a handle to a ByteString (e.g. `stdout` or `stderr`).
 redirectHandle :: Handle -> IO a -> IO (String, a)
-redirectHandle origHandle action = usingReadEnd origHandle $ \readEnd terminateOutput -> do
-    s <- hGetContents readEnd
-    r <- action
-    terminateOutput
-    force s
-    return (s, r)
+redirectHandle origHandle action = withSystemTempFile "interprocess.out" $ \_ tempFile -> do
+    start <- hGetPosn tempFile
+    hGetEncoding origHandle >>= \case
+      Just e  -> hSetEncoding tempFile e
+      Nothing -> hSetBinaryMode tempFile True
+    hSetBuffering tempFile NoBuffering
+    hSetNewlineMode tempFile noNewlineTranslation
+    r <- bracket (redirect $ hGetWriteEnd tempFile) unredirect (const action)
+    hFlushAll tempFile
+    r `seq` hSetPosn start
+    out <- hGetContents tempFile
+    force out
+    return (out, r)
   where
+    redirect tempFile = do
+      origDup <- hDuplicate origHandleOut
+      hDuplicateTo tempFile origHandleOut
+      return origDup
+
+    unredirect origDup = hDuplicateTo origDup origHandleOut >> hClose origDup
+
+    origHandleOut = hGetWriteEnd origHandle
+
     force []     = pure ()
     force (_:cs) = force cs
 
-usingReadEnd :: Handle -> (Handle -> IO () -> IO a) -> IO a
-usingReadEnd origHandle action = withCompatiblePipe origHandle $ \(readEnd, writeEnd) ->
-    bracket (redirect writeEnd) unredirect (const $ action readEnd (hClose origHandle >> hClose writeEnd))
-  where
-    redirect writeEnd = do
-      origDup <- hDuplicate origHandle
-      hDuplicateWriteEndTo writeEnd origHandle
-      return origDup
-
-    unredirect origDup = hDuplicateTo origDup origHandle
-
-hDuplicateWriteEndTo :: Handle -> Handle -> IO ()
-hDuplicateWriteEndTo h1@FileHandle{} (DuplexHandle p _ w) = hDuplicateTo h1 (FileHandle p w)
-hDuplicateWriteEndTo (DuplexHandle p _ w) h2@FileHandle{} = hDuplicateTo (FileHandle p w) h2
-hDuplicateWriteEndTo h1 h2 = hDuplicateTo h1 h2
-
-withCompatiblePipe :: Handle -> ((Handle, Handle) -> IO a) -> IO a
-withCompatiblePipe origHandle action = do
-    hFlush origHandle
-    encoding <- hGetEncoding origHandle
-    newline <- hGetNewlineMode origHandle
-    buffering <- hGetBuffering origHandle
-
-    let applyModes h = do
-          case encoding of
-            Just e  -> hSetEncoding h e
-            Nothing -> hSetBinaryMode h True
-          hSetNewlineMode h newline
-          hSetBuffering h buffering
-
-        closeTempHandles (readEnd, writeEnd)
-          = hClose writeEnd >> hClose readEnd
-
-        useTempHandles hs@(readEnd, writeEnd)
-          = applyModes readEnd >> applyModes writeEnd >> action hs
-
-    bracket createPipe closeTempHandles useTempHandles
-
-hGetNewlineMode :: Handle -> IO NewlineMode
-hGetNewlineMode hdl =
-  withHandle_ "hGetNewlineMode" hdl $ \h_ ->
-    return $ NewlineMode
-      { inputNL = haInputNL h_
-      , outputNL = haOutputNL h_
-      }
+    hGetWriteEnd (DuplexHandle p _ w) = FileHandle p w
+    hGetWriteEnd h                    = h
